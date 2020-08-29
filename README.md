1ab6a0d04485a6d7954014967d9d1354b32b7b34
unknown <tatianymori@gmail.com>
2020-08-28 21:38:54 -0300

    Sistemas operacionais: fork

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Engenharia de Computação - UTFPR Pato Branco
 Materiais diversos de algumas disciplinas cursadas no curso de Engenharia de Computação - UTFPR Pato Branco
 
-
+#### 	:computer: Sistemas Operacionais 
+Durante o estudo dos fundamentos teóricos dos sistemas operacionais com enfase em seus aspectos, tais como, organização interna, estrutural e mecanismos de implementação foram desevolvidos alguns exercícios para o melhor entendimento do conteúdo.
+Tais, o uso do Fork.
 
 #### :page_facing_up: License
 
---
+++ fork/fork.c
@@ -0,0 +1,59 @@
+/*
+~Disciplina: Sistemas Operacionais
+~Acadêmica: Tatiany Keiko Mori
+Trabalho 1 - Usando as chamadas de sistema fork(), wait(), exit(), getpid() e getppid()
+ - Gerando a seguinte árvore:
+			T
+		   _|_
+          |   |
+	      A   Y
+          |
+	      I
+	      |
+	      N
+*/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void){
+	int status;
+    pid_t pid;
+    printf("proc-T, pid %d, ppid %d, acaba de ser criado\n", getpid(), getppid());
+    pid = fork();
+	if (pid == 0){    
+    printf("proc-A, pid %d, ppid %d, acaba de ser criado\n", getpid(), getppid());
+    pid=fork();	
+		if (pid == 0){
+      printf("proc-I, pid %d, ppid %d, acaba de ser criado\n", getpid(), getppid());
+			pid=fork();			
+			if (pid == 0){
+				printf("proc-N, pid %d, ppid %d, acaba de ser criado\n", getpid(), getppid());
+				printf("proc-N, pid %d, morreu\n", getpid());
+			}
+			else{
+				wait(pid,&status,0);
+				printf("proc-I, pid %d, morreu\n", getpid());
+			}
+		}
+		else {
+			wait(pid,&status,0);
+			printf("proc-A, pid %d, morreu\n", getpid());
+		}
+	}
+	else{
+		wait(pid,&status,0);
+		pid=fork();
+		if (pid == 0) {
+			printf("proc-Y, pid %d, ppid %d, acaba de ser criado\n", getpid(), getppid());
+			printf("proc-Y, pid %d, morreu\n", getpid());
+		}
+		else{
+			wait(pid,&status,0);
+			printf("proc-T, pid %d, morreu\n", getpid());
+		}		
+	}
+	return 0;
+}
